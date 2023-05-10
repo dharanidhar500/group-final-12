81f790fcd535949cb169bddbc530001f2d484cbc
dharanidhar500 <74231747+dharanidhar500@users.noreply.github.com>
2023-05-10 12:49:29 +0530

    Update LogoutPage.java

--- src/main/java/org/example/LogoutPage.java
+++ src/main/java/org/example/LogoutPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class LogoutPage {
     @FindBy(xpath = "//div[@class='css-1dbjc4n r-172uzmj r-1pi2tsx r-1ny4l3l r-13qz1uu']")
     WebElement validuser;
-    @FindBy(xpath = "//span[normalize-space()='Log out @RishiKesh977439']")
+    @FindBy(xpath = "//span[normalize-space()='Log out @Ramkuma82374175']")
     WebElement logout;
 
     @FindBy(xpath = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span")
